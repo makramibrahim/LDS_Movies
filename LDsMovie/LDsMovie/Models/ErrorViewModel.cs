9590e017bfd1c8c12ef4ee749810b9ab9bdea6d2
Mak Ibrahim <makram3ibrahim@gmail.com>
2018-03-18 21:47:11 -0600

    get started with movie app

---
+++ LDsMovie/LDsMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LDsMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
